3ce506954f79e7319c5b618edd277b3d3da5282e
BeHappy <chinthakuntapavansai@gmail.com>
2024-07-07 18:24:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './data.json';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+    </div>
+  );
+}
+
+export default App;
